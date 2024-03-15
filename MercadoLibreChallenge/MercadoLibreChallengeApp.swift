69aec04fbefafbb8fde77b92260b76e9a9cadd16
Adelaida Gomez <adelaida.gomezvieco@teamwass.com>
2024-03-15 06:52:28 -0500

    Initial Commit

---
+++ MercadoLibreChallenge/MercadoLibreChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MercadoLibreChallengeApp.swift
+//  MercadoLibreChallenge
+//
+//  Created by Adelaida Gomez Vieco on 15/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MercadoLibreChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
